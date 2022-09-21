65c3a03f0f71dc2e69ffde6d599ddd6f9a11f786
yigitertek <yigitertek@hotmail.com>
2022-09-22 02:41:17 +0300

    V2

--- Pat Pat Ball/Assets/Scripts/Player.cs
+++ Pat Pat Ball/Assets/Scripts/Player.cs
@@ -4,20 +4,73 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject cam;
+    public GameObject vectorBack;
+    public GameObject vectorForward;
+
+    private Rigidbody rb;
+
     private Touch touch;
+    [Range(10,40)]
     public int speedModifier;
+    public int forwardSpeed;
+
+  
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
     public void Update()
     {
+        if (Variables.firstTouch == 1)
+        {
+            transform.position += new Vector3(0, 0, forwardSpeed * Time.deltaTime);
+            cam.transform.position += new Vector3(0, 0, forwardSpeed * Time.deltaTime);
+            vectorBack.transform.position += new Vector3(0, 0, forwardSpeed * Time.deltaTime);
+            vectorForward.transform.position += new Vector3(0, 0, forwardSpeed * Time.deltaTime);
+        }
+
+
+
+
+
+
+
+
         if (Input.touchCount >0)
         {
             touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Moved)
+            if (touch.phase == TouchPhase.Began)
             {
-                transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * speedModifier, transform.position.y,
-                                                  transform.position.z + touch.deltaPosition.y * speedModifier);
+                Variables.firstTouch = 1;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                rb.velocity = new Vector3(touch.deltaPosition.x * speedModifier * Time.deltaTime,
+                                         transform.position.y,
+                                         touch.deltaPosition.y * speedModifier * Time.deltaTime);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                rb.velocity = Vector3.zero;
+            }
+           
+        }
+    }
+    public GameObject[] FractureItems;
+    public void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.CompareTag("Obstacles"))
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            foreach (GameObject item in FractureItems)
+            {
+                item.GetComponent<SphereCollider>().enabled = true;
+                item.GetComponent<Rigidbody>().isKinematic = false;
             }
         }
     }
-
 }
